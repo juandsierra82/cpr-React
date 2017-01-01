798a9edbbd6fca35fc9160b1cdd8a664a52ba70d
juandsierra82 <juandsierra82@gmail.com>
2017-01-01 17:43:09 -0500

    set up the client js in different components

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Layout from "./components/Layout";
 
-class Layout extends React.Component{
-  constructor(){
-    super();
-    this.name = "Will"
-  }
-  render(){
-    return (
-      <h1>It doesnt {this.name} may works!!!</h1>
-    );
-  }
-}
 const app = document.getElementById("app");
 ReactDOM.render(<Layout/>, app);
