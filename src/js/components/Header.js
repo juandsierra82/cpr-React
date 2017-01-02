1a2617f5abaa2f31be429662f97fe176d03a9fdf
juandsierra82 <juandsierra82@gmail.com>
2017-01-01 19:36:41 -0500

    set up listeners

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import Title from "./header/Title"
 export default class Header extends React.Component{
 
+  handleChange(e){
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+
   render(){
+    console.log('the props', this.props)
     return (
-      <header>Header</header>
+      <div>
+      <Title title={this.props.title}/>
+      <input value={this.props.title} onChange={this.handleChange.bind(this)}/>
+      </div>
     );
   }
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,11 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component{
-
+  //state appropriate one layout
+  constructor(){
+    super();
+    this.state = {title: "Welcome"};
+  }
+  changeTitle(title){
+    this.setState({title})
+  }
   render(){
     return (
       <div>
-        <Header />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
         <Footer />
       </div>
     );
